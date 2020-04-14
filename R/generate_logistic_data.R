54cf2c6e734e0d0d84cb2742ba4f25344be8a734
Zane Billings <39701545+wz-billings@users.noreply.github.com>
2020-04-14 10:45:44 -0400

    Updated function framework.
    Now there are two major functions replacing the ~10 functions from the
    original structure, with options to replicate all of the previous functionality.
    
    Needs further bug testing.

--- R/generate_logistic_data.R
+++ R/generate_logistic_data.R
@@ -1,6 +1,33 @@
-generate_logistic_data <- function(P0, r, K = NULL, max_t, time_step, noise = 0,
-                                   method = "analytic", reduced = FALSE,
-                                   make_plot = FALSE) {
+#' Generate Example Logistic Growth Time-Series Data
+#'
+#' @param P0 Positive numeric; the initial population value.
+#' @param K Positive numeric; the carrying capacity. Defaults to 1,
+#'     which is the value to use if the model is dimensionless/relative.
+#' @param r Positive numeric; the intrinsic population growth rate.
+#' @param max_t Positive numeric; the maximum time step at which to evaluate
+#'     the model (note that the minimum time step is always 0).
+#' @param time_step Positive numeric; the size of steps to take between each
+#'     evaluation of the model. The vector of times will be generated as a
+#'     sequence from 0 to max_t in step-size of this parameter.
+#' @param noise Positive numeric; the amount of noise to add to the data, as a
+#'     proportion of the equilibrium value. E.g. setting this to some "s"
+#'     will add a normal random vector of noise to the data where all entries
+#'     are drawn from the normal distribution with mean 0 and sd s*K. If you
+#'     have an sd that you want to add manually, you will have to do the
+#'     algebra to get it as a proportion of K.
+#' @param method Character; chosen from "analytic" ("a"), "discretized" ("d"),
+#'     or "RK4" ("r"), which determines which method to use to generate the
+#'     data.
+#' @param make_plot Logical; if true, the function will generate a plot of the
+#'     generated time series for you.
+#'
+#' @return a data frame with columns "t", the time values at which the model was
+#'     evaluated, and "P", the population size at each time step.
+#' @export
+#'
+generate_logistic_data <- function(P0, K = 1, r, max_t, time_step, noise = 0,
+                                   method = "analytic", make_plot = FALSE,
+                                   as_df = FALSE) {
   # This function generates time-series data modeling logistic growth based on
   # the input parameters.
 
@@ -36,12 +63,30 @@ generate_logistic_data <- function(P0, r, K = NULL, max_t, time_step, noise = 0,
            numeric value.")
     }
 
-  # Check if dimensionless
-  if(reduced) {
-    # Choose method and generate data (dimensionless)
-    K = 1
-  } else {
-    # Choose method and generate data (2 parameter)
+  # Generate a list of time steps to solve for.
+  t <- seq(from = 0, to = max_t, by = time_step)
+
+  # Choose solving method and solve
+  if (method == "analytic" | method == "a") {
+    # use analytic method
+    P <- solve_logistic_equation(P_naught, K, r, t)
+    P[1] <- P_naught
+  } else if (method == "discretized" | method == "d") {
+    # use discrete method
+    P <- numeric(length(t))
+    P[1] <- P_naught
+    for (j in 2:length(t)) {
+      P[j] <- P[j - 1] + r*P[j - 1] * (1 - P[j - 1]/K) * time_step
+    }
+  } else if (method == "RK4" | method == "r") {
+    # use RK4 ode solver
+    solver_output <- deSolve::ode(
+      y = c(P = P_naught),
+      times = seq(from = 0, to = max_t, by = time_step),
+      func = calculate_logistic_derivative,
+      parms = c(r = r, K = K)
+    )
+    P <- as.data.frame(solver_output)$P
   }
 
   # Add noise if needed
@@ -51,6 +96,11 @@ generate_logistic_data <- function(P0, r, K = NULL, max_t, time_step, noise = 0,
   }
 
   # Make plot if requested
+  if (make_plot) {
+    plot(x = t, y = P, ylab = "P(t)", type = "b")
+  }
 
   # Prep and return outputs
+  output <- data.frame(t,P)
+  return(output)
 }
